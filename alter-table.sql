3d7350b25ae3ae5182ad9885bf8aa71b506998be
Kamshory, MT <kamshory@yahoo.com>
2023-08-07 06:58:33 +0700

    Update alter-table.sql

--- alter-table.sql
+++ alter-table.sql
@@ -15,3 +15,12 @@ ADD `admin_edit` VARCHAR(40) NULL DEFAULT NULL AFTER `admin_create`,
 ADD `ip_create` VARCHAR(50) NULL DEFAULT NULL AFTER `admin_edit`, 
 ADD `ip_edit` VARCHAR(50) NULL DEFAULT NULL AFTER `ip_create`; 
 
+
+ALTER TABLE `album` 
+ADD `time_create` TIMESTAMP NULL DEFAULT NULL AFTER `duration`, 
+ADD `time_edit` TIMESTAMP NULL DEFAULT NULL AFTER `time_create`, 
+ADD `admin_create` VARCHAR(40) NULL DEFAULT NULL AFTER `time_edit`, 
+ADD `admin_edit` VARCHAR(40) NULL DEFAULT NULL AFTER `admin_create`, 
+ADD `ip_create` VARCHAR(50) NULL DEFAULT NULL AFTER `admin_edit`, 
+ADD `ip_edit` VARCHAR(50) NULL DEFAULT NULL AFTER `ip_create`; 
+
