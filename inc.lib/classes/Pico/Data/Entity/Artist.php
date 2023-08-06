72d7c165716f2c31bcd7db42a375b7017b7ccf6c
Kamshory, MT <kamshory@yahoo.com>
2023-08-07 06:54:39 +0700

    Update Artist.php

--- inc.lib/classes/Pico/Data/Entity/Artist.php
+++ inc.lib/classes/Pico/Data/Entity/Artist.php
@@ -1,53 +1,84 @@
 <?php
+
 namespace Pico\Data\Entity;
 
 use Pico\DynamicObject\DynamicObject;
 
 /**
- * Artist
  * @Entity
- * @Table (name=artist)
+ * @JSON(property-naming-strategy=SNAKE_CASE)
+ * @Table(name="artist")
  */
 class Artist extends DynamicObject
-{    
-    /**
-     * Artis ID
-     *
-     * @var string
-     * @Column (name=artist_id)
-     * @Id
-     */
-    protected $artistId;
-    
-    /**
-     * Name
-     *
-     * @var string
-     * @Column (name=name)
-     */
-    protected $name;
+{
+	/**
+	 * @Id
+	 * @GeneratedValue(strategy=GenerationType.UUID)
+	 * @NotNull
+	 * @Column(name="artist_id", type="varchar(40)", length=40, nullable=false)
+	 * @var string
+	 */
+	protected $artistId;
 
-    /**
-     * Gender
-     *
-     * @var string
-     * @Column (name=gender)
-     */
-    protected $gender;
+	/**
+	 * @Column(name="name", type="varchar(100)", length=100, nullable=true)
+	 * @var string
+	 */
+	protected $name;
 
-    /**
-     * Birth day
-     *
-     * @var string
-     * @Column (name=birth_day)
-     */
-    protected $birthDay;
+	/**
+	 * @Column(name="gender", type="varchar(2)", length=2, nullable=true)
+	 * @var string
+	 */
+	protected $gender;
 
-    /**
-     * Active
-     *
-     * @var bool
-     * @Column (name=active)
-     */
-    protected $active;
+	/**
+	 * @Column(name="birth_day", type="date", nullable=true)
+	 * @var string
+	 */
+	protected $birthDay;
+
+	/**
+	 * @Column(name="time_create", type="timestamp", length=19, nullable=true, updatable=false)
+	 * @var string
+	 */
+	protected $timeCreate;
+
+	/**
+	 * @Column(name="time_edit", type="timestamp", length=19, nullable=true)
+	 * @var string
+	 */
+	protected $timeEdit;
+
+	/**
+	 * @Column(name="admin_create", type="varchar(40)", length=40, nullable=true, updatable=false)
+	 * @var string
+	 */
+	protected $adminCreate;
+
+	/**
+	 * @Column(name="admin_edit", type="varchar(40)", length=40, nullable=true)
+	 * @var string
+	 */
+	protected $adminEdit;
+
+	/**
+	 * @Column(name="ip_create", type="varchar(50)", length=50, nullable=true, updatable=false)
+	 * @var string
+	 */
+	protected $ipCreate;
+
+	/**
+	 * @Column(name="ip_edit", type="varchar(50)", length=50, nullable=true)
+	 * @var string
+	 */
+	protected $ipEdit;
+
+	/**
+	 * @Column(name="active", type="tinyint(1)", length=1, default_value="1", nullable=true)
+	 * @DefaultColumn(value="1")
+	 * @var bool
+	 */
+	protected $active;
+
 }
