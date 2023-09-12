d83709e69aef12556289650e32adbaedb28bba58
Kamshory, MT <kamshory@yahoo.com>
2023-09-13 06:18:09 +0700

    Add dependency

--- inc.lib/vendor/composer/autoload_real.php
+++ inc.lib/vendor/composer/autoload_real.php
@@ -22,13 +22,15 @@ class ComposerAutoloaderInit2219d434bd9928e433eb450fe0d2c608
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit2219d434bd9928e433eb450fe0d2c608', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit2219d434bd9928e433eb450fe0d2c608', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInit2219d434bd9928e433eb450fe0d2c608::getInitializer($loader));
         } else {
@@ -63,11 +65,16 @@ class ComposerAutoloaderInit2219d434bd9928e433eb450fe0d2c608
     }
 }
 
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
 function composerRequire2219d434bd9928e433eb450fe0d2c608($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        require $file;
-
         $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
--- inc.lib/vendor/composer/autoload_static.php
+++ inc.lib/vendor/composer/autoload_static.php
@@ -7,6 +7,7 @@ namespace Composer\Autoload;
 class ComposerStaticInit2219d434bd9928e433eb450fe0d2c608
 {
     public static $files = array (
+        '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
     );
 
@@ -39,12 +40,17 @@ class ComposerStaticInit2219d434bd9928e433eb450fe0d2c608
         ),
     );
 
+    public static $classMap = array (
+        'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit2219d434bd9928e433eb450fe0d2c608::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit2219d434bd9928e433eb450fe0d2c608::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit2219d434bd9928e433eb450fe0d2c608::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit2219d434bd9928e433eb450fe0d2c608::$classMap;
 
         }, null, ClassLoader::class);
     }
