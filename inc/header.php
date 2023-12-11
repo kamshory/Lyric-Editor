bf6e89d8287090174386d89c5e6a183c09d250e3
Kamshory, MT <kamshory@yahoo.com>
2023-12-11 10:08:15 +0700

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
   <title>Music Creator</title>
   <link rel="shortcut icon" type="image/png" href="assets/images/logos/favicon.png" />
   
